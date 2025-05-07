7dfe3a04697d4a6c0ead18ae35d94e8f40a273bf
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-05-07 13:19:05 +0700

    add data and card accordion

--- frontend/src/app/(landingPage)/home/page.jsx
+++ frontend/src/app/(landingPage)/home/page.jsx
@@ -1,11 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "@/components/Navbar";
+import FAQ from "@/data/FAQ";
+import CardAccordion from "@/components/card/CardAccordion";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        {FAQ.map((data, index) => (
+          <CardAccordion
+            key={index}
+            question={data.question}
+            answer={data.answer}
+          />
+        ))}
+      </div>
     </div>
   );
 };
