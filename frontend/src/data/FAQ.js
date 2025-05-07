ced06ab02f0ad85fbd945534fe6e1b33cd51980e
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-05-07 13:19:13 +0700

    add data FAQ

---
+++ frontend/src/data/FAQ.js
@@ -0,0 +1,37 @@
+const faqData = [
+  {
+    id: 1,
+    question:
+      "Jenis amal apa yang bisa saya donasikan, dan bagaimana cara menemukannya?",
+    answer:
+      "Ada banyak jenis badan amal, mulai dari organisasi lokal hingga organisasi internasional besar. Anda bisa berdonasi kepada badan amal yang fokus pada tujuan tertentu, seperti kesejahteraan hewan.",
+  },
+  {
+    id: 2,
+    question:
+      "Apakah donasi saya dapat dikurangkan dari pajak, dan bagaimana cara mengklaimnya di pajak saya?",
+    answer:
+      "Tergantung pada hukum pajak di negara Anda. Beberapa donasi bisa dikurangkan dari pajak jika Anda menyumbang ke organisasi yang diakui. Simpan bukti donasi untuk keperluan pelaporan pajak.",
+  },
+  {
+    id: 3,
+    question: "Bisakah saya berdonasi secara anonim?",
+    answer:
+      "Ya, banyak platform donasi menyediakan opsi untuk berdonasi tanpa menampilkan nama Anda kepada publik atau penerima donasi.",
+  },
+  {
+    id: 4,
+    question:
+      "Berapa persen dari donasi saya yang sebenarnya digunakan oleh badan amal untuk keperluan administratif?",
+    answer:
+      "Setiap badan amal berbeda-beda, namun sebagian besar akan menginformasikan transparansi alokasi dana. Idealnya, sebagian besar dana digunakan langsung untuk program bantuan.",
+  },
+  {
+    id: 5,
+    question: "Bisakah saya menyumbangkan barang atau jasa alih-alih uang?",
+    answer:
+      "Beberapa organisasi menerima donasi dalam bentuk barang atau jasa, tergantung pada kebutuhan mereka. Cek syarat dan ketentuan masing-masing badan amal.",
+  },
+];
+
+export default faqData;
