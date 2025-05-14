1b161fdebea3ec8231ea09b45dc6b319846cbe9d
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-05-14 14:40:27 +0700

    add donasi data

---
+++ frontend/src/data/donasiData.js
@@ -0,0 +1,34 @@
+export const donasiData = [
+  {
+    slug: "program-air-bersih",
+    judul: "Program Air Bersih",
+    deskripsiPendek:
+      "Membantu kelurahan desa Bojongsoang untuk menemukan air bersih",
+    lokasi: "Bojongsoang, Bandung",
+    batasAkhir: "17 Juni 2025",
+    organizer: "HealtyOrganization",
+    totalTerkumpul: 2000000,
+    persentase: 72,
+    gambar: "/img/foto_kegiatan.jpg",
+    isiKonten: [
+      "Program ini bertujuan untuk menyediakan akses air bersih bagi warga desa Bojongsoang.",
+      "Kami bekerja sama dengan komunitas lokal dan relawan untuk melakukan pengeboran dan distribusi air bersih.",
+    ],
+  },
+  {
+    slug: "bantu-anak-sekolah",
+    judul: "Bantu Anak Sekolah",
+    deskripsiPendek:
+      "Membantu anak-anak kurang mampu mendapatkan perlengkapan sekolah",
+    lokasi: "Cimahi, Jawa Barat",
+    batasAkhir: "30 Juli 2025",
+    organizer: "PendidikanUntukSemua",
+    totalTerkumpul: 500000,
+    persentase: 25,
+    gambar: "/img/bantu_sekolah.jpg",
+    isiKonten: [
+      "Donasi ini akan digunakan untuk membeli buku, seragam, dan alat tulis bagi anak-anak dari keluarga prasejahtera.",
+      "Kami berharap dengan bantuan ini, semangat belajar anak-anak akan meningkat.",
+    ],
+  },
+];
