9343c42e53def970a03b8a2a0d516448afeb66d5
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-06-12 16:58:48 +0700

    add logic

--- frontend/src/app/(donor)/donasi/page.jsx
+++ frontend/src/app/(donor)/donasi/page.jsx
@@ -2,8 +2,26 @@ import DonationCard from "@/components/card/DonationCard";
 import React from "react";
 import { dataCampaign } from "@/data/campaign";
 import { dataUsers } from "@/data/users";
+import { hitungPersentaseDonasi } from "@/lib/utils/campaign-helpers";
 
 const Donasi = () => {
+  const allDonations = dataCampaign.flatMap((campaign) =>
+    campaign.pengajuanDonasi.map((donasi) => ({
+      ...donasi,
+      campaign: {
+        ...campaign,
+        namaCampaign: donasi.judulCampaign,
+        gambarBuktiCampaign: donasi.gambarBuktiCampaign,
+        deskripsi: donasi.deskripsi,
+        progress: hitungPersentaseDonasi(
+          donasi.id_donasi,
+          dataCampaign,
+          dataUsers
+        ),
+      },
+    }))
+  );
+
   return (
     <>
       <section className="px-6 md:px-[110px] py-4 container mx-auto">
@@ -18,16 +36,9 @@ const Donasi = () => {
       </section>
 
       <section className="mt-6 px-6 md:px-[110px] py-4 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 items-stretch">
-        {dataCampaign.map((campaign) =>
-          campaign.pengajuanDonasi.map((donasi) => (
-            <DonationCard
-              key={donasi.id_donasi}
-              idDonasi={donasi.id_donasi}
-              dataCampaign={dataCampaign}
-              userDonasi={dataUsers}
-            />
-          ))
-        )}
+        {allDonations.map((donasi) => (
+          <DonationCard key={donasi.id_donasi} campaign={donasi.campaign} />
+        ))}
       </section>
     </>
   );
